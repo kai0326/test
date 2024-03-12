7744ab243b21ac0774db4ad02eed7b7fdd278ff1
kai0326 <yungchen@asia.edu.tw>
2024-03-12 15:30:49 +0800

    0312

--- mblog/settings.py
+++ mblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c&6nh!sq4zdoud!v=g_sf%8=-trga3n092p5-i933k=4n4^9-_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
